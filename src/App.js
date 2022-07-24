3bba4b5ebf6e325aad5c9a9c51badeb4af5344bb
RacolBan <kain.199x@gmail.com>
2022-07-24 10:06:04 +0700

    upload app componet

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import logo from './logo.svg';
 import './App.css';
 
-// template + logic
+// template + logic 
 const App = () => {
+  let obj = { name: "khai", age: 23 }
+  const link = `https://github.com/Racol-Din/React-Hook`;
   return (
     <div className="App">
+      {console.log(">>> check", { obj })}
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h2>
-          Learn React with Racol
+          Learn React with Racol {obj.age}
         </h2>
+        <a href={link} target="_blank"> GITHUB</a>
 
       </header>
     </div>
