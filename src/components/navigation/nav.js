4461fbe479c12e1bd696f172f6d1b5ad3e7dae5f
RacolBan <kain.199x@gmail.com>
2022-07-31 05:42:34 +0700

    upload multiple components

---
+++ src/components/navigation/nav.js
@@ -0,0 +1,18 @@
+import './nav.css'
+
+const Navigation = () => {
+    return (
+        <>
+            <div className="topnav">
+                <a className="active" href="#home">Home</a>
+                <a href="#news">News</a>
+                <a href="#contact">Contact</a>
+                <a href="#about">About</a>
+            </div>
+
+
+        </>
+    );
+}
+
+export default Navigation
