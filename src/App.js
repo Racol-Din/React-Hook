312e2ee9b7a0f4bb12db6dd23d446f3ada33832a
RacolBan <kain.199x@gmail.com>
2022-08-03 04:15:41 +0700

    understanding arg function

--- src/App.js
+++ src/App.js
@@ -3,15 +3,16 @@ import './App.css';
 // template + logic 
 const App = () => {
   let obj = { name: "khai", age: 23 }
-  const link = `https://github.com/Racol-Din/React-Hook`;
+  const handleClick = (a) => {
+    console.log(">>>Click me", a);
+  }
   return (
     <div className="App">
       <header className="App-header">
         <h2>
           Learn React with Racol {obj.age}
         </h2>
-        <p> {JSON.stringify(obj)}</p>
-        <a href={link} target="_blank" rel="noreferrer"> GITHUB</a>
+        <button type="button" onClick={(event) => handleClick(event)}>Click</button>
 
       </header>
     </div>
