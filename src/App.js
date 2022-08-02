aabf328cecb8d0c2285c1eff7688de91bd9bd366
RacolBan <kain.199x@gmail.com>
2022-08-03 05:38:38 +0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,20 +1,26 @@
 import './App.css';
 import Navigation from './components/navigation/nav';
-// template + logic 
+import { useState } from 'react';
 const App = () => {
   let obj = { name: "khai", age: 23 }
+  let [name, setName] = useState('khai')
+  let [address, setAddress] = useState('')
   const handleClick = (event) => {
-    console.log(`clear alll`, event.target.value);
-
+    setName(address)
+    console.log(`>> click me`, address);
+  }
+  const handleOnChangeInput = (event) => {
+    setAddress(event.target.value)
+    console.log(event.target.value);
   }
   return (
     <div className="App">
       <Navigation />
       <header className="App-header">
         <h2>
-          Learn React with Racol {obj.age}
+          Learn React with Racol {name}
         </h2>
-        <input type="text" value={obj.name} onClick={(event) => handleClick(event)} />
+        <input type="text" value={address} onChange={(event) => { setAddress(event.target.value) }} />
         <button type="button" onClick={(event) => handleClick(event)}>Click</button>
 
       </header>
