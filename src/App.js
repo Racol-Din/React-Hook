32162fe789a1a40816373ed3b80485b3aff39ed5
RacolBan <kain.199x@gmail.com>
2022-08-03 04:48:47 +0700

    event.target.value

--- src/App.js
+++ src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
-
+import Navigation from './components/navigation/nav';
 // template + logic 
 const App = () => {
   let obj = { name: "khai", age: 23 }
-  const handleClick = (a) => {
-    console.log(">>>Click me", a);
+  const handleClick = (event) => {
+    console.log(`clear alll`, event.target.value);
+
   }
   return (
     <div className="App">
+      <Navigation />
       <header className="App-header">
         <h2>
           Learn React with Racol {obj.age}
         </h2>
+        <input type="text" value={obj.name} onClick={(event) => handleClick(event)} />
         <button type="button" onClick={(event) => handleClick(event)}>Click</button>
 
       </header>
